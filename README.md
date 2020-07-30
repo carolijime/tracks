36b54beb6ab88fe616d261f8500772cda0a7c6cf
Carolina <carolijime@hotmail.com>
2020-07-29 18:15:08 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+change ngok
+ngrok http 3000
+
+copy url and paste it in tracker,js
+
+run in C:\Users\carol\DevPro\ReactNativeProjects\track-server> 
+npm run dev
+to activate mongodb
+
+NOTE: mongo db has to have the IP address in the white list in order to work
---
+++ src - Copy/screens/Generic.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Text, StyleSheet, View, Button, Image } from 'react-native';
+
+const ListScreen = () => {
+
+    return (<View>
+                <Text>List Screen</Text>
+            </View>
+    );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default ListScreen;
